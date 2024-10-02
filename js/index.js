901b1f584a4aa8346c1577f929429b3b50206aa8
Dhirajpatil7721 <dhirajpatil7721@gmail.com>
2024-10-02 13:24:14 +0530

    some msg

---
+++ js/index.js
@@ -0,0 +1,6 @@
+let log = document.getElementById('loged')
+
+
+log.addEventListener("click", function() {
+  window.location.href = "/html/login.html";
+});
---
+++ js/product_details.js
@@ -0,0 +1,10 @@
+const buybtn = document.querySelector('#buy');
+
+
+buybtn.addEventListener('click',()=>{
+    alert("ok")
+    window.location.href='/html/cart.html'
+})
+
+
+
